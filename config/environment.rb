5722983fd96365a3a705e84df021fd4bde6ebe36
midwhite <white_hero_0204@yahoo.co.jp>
2016-09-16 04:59:21 +0000

    fix config/environment

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,11 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Credentials' => 'true',
-    'Access-Control-Allow-Origin' => 'https://clue-web.herokuapp.com/',
-    'Access-Control-Request-Method' => '*'
-}
+# config.action_dispatch.default_headers = {
+#     'Access-Control-Allow-Credentials' => 'true',
+#     'Access-Control-Allow-Origin' => 'https://clue-web.herokuapp.com/',
+#     'Access-Control-Request-Method' => '*'
+# }
 
 # Initialize the Rails application.
 Rails.application.initialize!
